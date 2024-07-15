0d8131533c4ac5b23ca9c951d290ac275a4cfa5b
junfengl95 <junfeng011995@gmail.com>
2024-07-15 19:53:01 +0800

    Completed remove-book button and read-status-toggle-button

--- library.js
+++ library.js
@@ -9,6 +9,11 @@ document.addEventListener('DOMContentLoaded', () => {
         this.hasRead = hasRead;
     }
 
+    // Prototype method to toggle the read status of Book
+    Book.prototype.toggleReadStatus = function() {
+        this.hasRead = !this.hasRead; // swap true and false
+    }
+
     // Add an infor method to the Book prototype
     Book.prototype.info = function () {
         return `${this.title} by ${this.author}, ${this.pages}, ${this.hasRead ? 'read' : 'not read yet'}`;
@@ -17,16 +22,30 @@ document.addEventListener('DOMContentLoaded', () => {
     function addBookToLibrary(title, author, pages, hasRead) {
         const newBook = new Book(title, author, pages, hasRead);
         myLibrary.push(newBook);
-        displayBooks();
+        // console.log('New book added: ', newBook.info());
+        // console.log('Current library:', myLibrary)
+        displayLibrary();
+    }
+
+    // Function to remove a Book from the library
+    function removeBookFromLibrary(index){
+        myLibrary.splice(index, 1);
+        displayLibrary();
+    }
+
+    // Function to toggle the read status of a book
+    function toggleReadStatus(index) {
+        myLibrary[index].toggleReadStatus();
+        displayLibrary();
     }
 
 
-    function displayBooks() {
+    function displayLibrary() {
 
         const bookshelf = document.getElementById('bookshelf');
         bookshelf.innerHTML = "";
 
-        myLibrary.forEach(book => {
+        myLibrary.forEach((book, index)=> {
             // console.log(book.info())
             // Create a Card element
             const card = document.createElement('div');
@@ -49,8 +68,24 @@ document.addEventListener('DOMContentLoaded', () => {
 
             // Create read status element
             const hasRead = document.createElement('p');
-            hasRead.textContent = book.hasRead ? 'Status: Read' : "Status: Not read";
+            hasRead.textContent = book.hasRead ? 'Status: Have Read' : "Status: Not Read";
             card.appendChild(hasRead);
+
+            // Create remove button element
+            const removeButton = document.createElement('button');
+            removeButton.textContent = 'Remove';
+            removeButton.addEventListener('click', () => {
+                removeBookFromLibrary(index);
+            });
+            card.appendChild(removeButton);
+
+            // Create toggle Read status button element
+            const toggleButton = document.createElement('button');
+            toggleButton.textContent = book.hasRead ? 'Not Read' : "Have Read";
+            toggleButton.addEventListener('click', () => {
+                toggleReadStatus(index);
+            });
+            card.appendChild(toggleButton);
 
             // Append the card to bookshelf
             bookshelf.appendChild(card);
@@ -62,44 +97,43 @@ document.addEventListener('DOMContentLoaded', () => {
     addBookToLibrary('1984', 'George Orwell', 328, true);
     addBookToLibrary('To Kill a Mockingbird', 'Harper Lee', 281, false)
 
-    displayBooks();
-
     // Dialog modal and form handling
     const dialog = document.getElementById('bookDialog');
     const btn = document.getElementById('addBook');
-    const form = document.getElementsByClassName(`bookform`);
+    const form = document.getElementById('bookForm')
     const cancelBtn = document.getElementById('cancelButton')
-    const span = document.getElementsByClassName('close')[0];
+    const span = document.querySelector('.close');
+    const submitBtn = document.getElementById('submitButton');
 
     // When user clicks on the button, open the modal
-    btn.onclick = function() {
+    btn.onclick = function () {
         dialog.showModal(); // open the dialog
     }
 
     // When user clicks on <span> (x), close the modal
-    span.onclick = function() {
+    span.onclick = function () {
         dialog.close(); // close the dialog
     }
 
-    cancelBtn.onclick = function() {
+    cancelBtn.onclick = function () {
         dialog.close();
     }
 
     // When user clicks anywhere outside the modal. close it
-    // window.onclick = function(event) {
-    //     if (event.target == dialog){
-    //         modal.style.display = 'none';
-    //     }
-    // }
+    window.onclick = function(event) {
+        if (event.target == dialog){
+            dialog.close();
+        }
+    }
 
     //When form is submitted, add new book to library
-    form.onsubmit = function(event) {
+    submitBtn.onclick = function (event) {
         event.preventDefault(); // without action, default action will redirect to page
 
         const title = document.getElementById('title').value;
         const author = document.getElementById('author').value;
         const pages = document.getElementById('pages').value;
-        const hasRead = document.getElementById('hasRead').ariaChecked;
+        const hasRead = document.querySelector('input[name="hasRead"]:checked').value === 'true';
 
         addBookToLibrary(title, author, pages, hasRead);
 
@@ -107,5 +141,4 @@ document.addEventListener('DOMContentLoaded', () => {
         dialog.close();
         form.reset();
     }
-
 })  
